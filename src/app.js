cc80aafdfb864c846608ee0c3b7e3d2f305694f9
MichalSzopa <szopqa96@gmail.com>
2017-08-26 12:51:30 +0200

    Client added, basic folders structure

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const router =require('./routers/weatherRoutes.js');
-
 
 const app = express();
 
-app.use(bodyParser.json());
-
-
 router.weatherRoutes(app);
-
 
 const port = process.env.PORT || 8888;
 app.listen(port,()=>console.log(`Server started on port ${port}`));
+
+module.exports={
+	app
+};
