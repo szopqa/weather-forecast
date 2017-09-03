370f100f1c76cfb54b475c3e01c1c848f24914d4
MichalSzopa <szopqa96@gmail.com>
2017-09-04 01:50:11 +0200

    Modules cleanup, removal of testing routes

--- src/server/app.js
+++ src/server/app.js
@@ -8,11 +8,6 @@ router.weatherRoutes(app);
 const port = process.env.PORT || 8888;
 app.listen(port,()=>console.log(`Server started on port ${port}`));
 
-//AJAX testing purpose
-app.get('/',(req,res) => {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.send('ggg');
-});
 
 module.exports={
 	app
