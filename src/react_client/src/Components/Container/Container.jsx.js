af5b3bf8ce65987b9445b75a6cbd719343513351
MichalSzopa <szopqa96@gmail.com>
2017-09-23 10:41:11 +0200

    Project cleanup, basic layout concept

---
+++ src/react_client/src/Components/Container/Container.jsx.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+class Container extends React.Component{
+
+	constructor(props){
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render(){
+		return(
+			<div className="container">
+
+			</div>
+		);
+	}
+}
+
+export default Container;
+
---
+++ src/react_client/src/Components/Header/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navigationbar extends React.Component{
+	render(){
+		return (
+			<div className="header">
+				HEADER
+			</div>
+		)
+	}
+}
+
+export default Navigationbar;
